45b38631dfb96c5c6a0a4b2c7b38be40cd899f46
Artur Ratajczak <artur.ratajczak1@gmail.com>
2018-10-22 09:15:54 -0700

    Initial Commit

---
+++ Stalk/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stalk
+//
+//  Created by Artur on 22/10/2018.
+//  Copyright © 2018 Artur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
