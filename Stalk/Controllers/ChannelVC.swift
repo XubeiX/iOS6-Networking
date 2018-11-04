080455654b09c86575e28e247ca8a14a3073d575
Artur Ratajczak <artur.ratajczak1@gmail.com>
2018-11-04 08:37:48 -0800

    User login after account create

--- Stalk/Controllers/ChannelVC.swift
+++ Stalk/Controllers/ChannelVC.swift
@@ -13,11 +13,12 @@ class ChannelVC: UIViewController {
     let SWRevealViewMargin: CGFloat = 64
     
     @IBOutlet weak var LoginBtn: UIButton!
+    @IBOutlet weak var userAvaterImg: CircleImage!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.revealViewController()?.rearViewRevealWidth = self.view.frame.size.width - SWRevealViewMargin
-        // Do any additional setup after loading the view.
+        NotificationCenter.default.addObserver(self, selector: #selector(userDataDidChange(_:)), name: NOTIFY_USER_DATA_DID_CHANGE, object: nil)
     }
     
     @IBAction func LoginBtnPressed(_ sender: Any) {
@@ -26,4 +27,15 @@ class ChannelVC: UIViewController {
     
     @IBAction func prepareForUnwind(segue: UIStoryboardSegue){}
 
+    @objc func userDataDidChange(_ notification: Notification) {
+        if AuthService.instance.isLoggedIn {
+            LoginBtn.setTitle("\(UserDataService.instance.name)", for: .normal)
+            userAvaterImg.image = UIImage(named: UserDataService.instance.avatarName)
+            userAvaterImg.backgroundColor = UserDataService.instance.getUIColor(components: UserDataService.instance.avatarColor)
+        } else {
+            LoginBtn.setTitle("Login", for: .normal)
+            userAvaterImg.image = UIImage(named: "menuProfileIcon")
+            userAvaterImg.backgroundColor = UIColor.clear
+        }
+    }
 }
